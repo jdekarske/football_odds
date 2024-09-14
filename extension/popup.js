5a46a3a9f7817dc382b0d71804d8fa5bb4c6d2d4
Jason Dekarske <dekarskej@gmail.com>
2024-09-13 19:11:15 -0700

    extension works!

--- extension/popup.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('readValues').addEventListener('click', () => {
-    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-        chrome.tabs.executeScript(tabs[0].id, { file: 'content.js' }, (results) => {
-            document.getElementById('output').textContent = JSON.stringify(results[0], null, 2);
-        });
-    });
-});
